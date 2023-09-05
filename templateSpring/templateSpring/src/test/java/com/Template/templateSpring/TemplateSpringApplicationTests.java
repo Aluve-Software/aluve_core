e24a20628799134a9c67f5c284d2b08c5d58ce8f
Tshepang Mamanyuwa <132703162+TshepangMa@users.noreply.github.com>
2023-09-05 17:50:57 +0200

    Add files via upload
    Template For Springboot applications, this is a barebone application all it has is dependencies needed to create the backend for web-based applications and communications with databases as well as spring security for api authentication.

---
+++ templateSpring/templateSpring/src/test/java/com/Template/templateSpring/TemplateSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Template.templateSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
